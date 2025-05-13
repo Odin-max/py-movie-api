0a575a1604aca47b77b467f25278aa03247139a7
Odin-max <meh84741@gmail.com>
2025-05-13 21:32:02 +0300

    3

--- new_project/cinema/urls.py
+++ new_project/cinema/urls.py
@@ -6,5 +6,5 @@ from .views import MovieListCreateView, MovieRetrieveUpdateDestroyView
 
 urlpatterns = [
     path("movies/", MovieListCreateView.as_view(), name="movie-list-create"),
-    path("movies/<int:pk>", MovieRetrieveUpdateDestroyView.as_view(), name="movie-detail")
+    path("movies/<int:pk>/", MovieRetrieveUpdateDestroyView.as_view(), name="movie-detail")
 ]
